a6b21b7ac4bcc55f7e24ff78200ad5ebe1a9e894
runmathjava <85817730+runmathjava@users.noreply.github.com>
2021-06-29 22:54:38 -0600

    initial commit

---
+++ src/main/java/com/seongkim/hieveryone/Hi.java
@@ -0,0 +1,15 @@
+/*
+ *@author Seong Kim
+ *email: aboxinaobx@hotmail.com
+ *date: 2021-60-28
+ *purpose: My first Hi Everyone program in an IDE
+ */
+package com.seongkim.hieveryone;
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi, Everyone!");
+        System.out.println("How are you?");
+        System.out.println("Have a nice day!");
+    }
+}
